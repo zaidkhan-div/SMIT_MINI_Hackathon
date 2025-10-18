04684e44afa0fb2c053c028134125fc208c3a13b
zaidkhan-div <zaidscestudent@gmail.com>
2025-10-18 23:14:37 +0500

    initializing

---
+++ src/app/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <h1 className='text-2xl font-normal text-red-500'>Hello world</h1>
+    </div>
+  )
+}
+
+export default page
